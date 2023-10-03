5d80457dd57676962a9e960dc87306b1ad08297f
Elliott1807 <144172739+Elliott1807@users.noreply.github.com>
2023-10-03 10:43:09 -0400

    code arduino
    arduinofew

--- sosarduino.ino
+++ sosarduino.ino
@@ -25,13 +25,47 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13,OUTPUT);
+
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, 1);
+  delay(700);
+  digitalWrite(13, 0);
+  delay(500);
+  digitalWrite(13, 1);
+  delay(700);
+  digitalWrite(13, 0);
+  delay(500);
+  digitalWrite(13, 1);
+  delay(700);
+  digitalWrite(13, 0);
+  delay(500);
+  digitalWrite(13, 1);
+  delay(200);
+  digitalWrite(13, 0);
+  delay(500);
+  digitalWrite(13, 1);
+  delay(200);
+  digitalWrite(13, 0);
+  delay(500);
+  digitalWrite(13, 1);
+  delay(200);
+  digitalWrite(13, 0);
+  delay(500);
+  digitalWrite(13, 1);
+  delay(700);
+  digitalWrite(13, 0);
+  delay(500);
+  digitalWrite(13, 1);
+  delay(700);
+  digitalWrite(13, 0);
+  delay(500);
+  digitalWrite(13, 1); 
+  delay(700);
+  digitalWrite(13, 0);
+  delay(500);
 }
+

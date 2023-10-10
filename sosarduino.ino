c86a9e4dc6679d85de8dfda69882babea23cdb17
Elliott1807 <144172739+Elliott1807@users.noreply.github.com>
2023-10-10 14:56:08 -0400

    servo moter

--- sosarduino.ino
+++ sosarduino.ino
@@ -1,26 +1,39 @@
 
+#include <Servo.h>
+
+Servo myServo;
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  myServo.attach(11,1000,2000);
+  myServo.write(90);
 
 }
 
 void blinkOnce(int time){
   digitalWrite(LED_BUILTIN, HIGH);
+  myServo.write(0);
   delay(time);
   digitalWrite(LED_BUILTIN, LOW);
+  myServo.write(180);
   delay(time);
+  
+}
+
+void blinkSOS() {
+  blinkOnce(700);
+  blinkOnce(700);
+  blinkOnce(700);
+  blinkOnce(300);
+  blinkOnce(300);
+  blinkOnce(300);
+  blinkOnce(700);
+  blinkOnce(700);
+  blinkOnce(700);
 }
 
 void loop() {
-  blinkOnce(700);
-  blinkOnce(700);
-  blinkOnce(700);
-  blinkOnce(300);
-  blinkOnce(300);
-  blinkOnce(300);
-  blinkOnce(700);
-  blinkOnce(700);
-  blinkOnce(700);
+  blinkSOS();
 }
 
